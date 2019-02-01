68703a8c8d9193f40bc42a5237507e0471689699
Patrick Sanfiel <patricksanfiel@gmail.com>
2019-02-01 18:07:48 -0500

    JS refactoring

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,10 +1,15 @@
 window.onload = () => {
+    // Clock Elements
     const SECOND_HAND = document.querySelector("#second-hand")
     const MINUTE_HAND = document.querySelector("#minute-hand")
     const HOUR_HAND = document.querySelector("#hour-hand")
+    
+    // Interface Elements
+    const SUBDIV_TOGGLE_BUTTONS = document.getElementById("interface-subdiv-container");//Element which will hold an event listener, expanding each of the interface subdivs when the corresponding toggle button is clicked
+    const COLOR_BUTTONS = document.getElementsByClassName("color-button")
+
 
     function setTime(){
-        // console.log(CLOCK_HANDS)
         const TODAY = new Date()
         
         // Seconds Variables
@@ -26,7 +31,7 @@ window.onload = () => {
         HOUR_HAND.style.transform = `rotate(${hoursDegrees}deg)`
     }
 
-    const CHANGE_CLOCK_COLORS = document.getElementById("interface-subdiv-container");//Element which will hold an event listener, expanding each of the interface subdivs when the corresponding toggle button is clicked
+    
     function toggleColorPanels(e) {
         if(e.target !== e.currentTarget && !e.target.classList.contains("color-button") && e.target.classList.contains("show-button-toggle")){ //We want to target the buttons inside the #interface-subdiv-container div and not the div itself. We also want to make sure we're not targeting any of the color change buttons, otherwise all the other panels would expand every time a user changed a color. Finally, we want to make sure that only visible buttons are clickable, which we do by ensuring that our clicked element has the show-button-toggle class. All of the interface panel toggle buttons start out with this class, and when one is clicked, it is removed from all of the others. When that same toggle button is clicked again to collapse its panel, the show-button-toggle class is reapplied to all of the toggle buttons once again.
             let toggleButton = e.target;
@@ -49,33 +54,36 @@ window.onload = () => {
     }
     
     
-    const COLOR_BUTTONS = document.getElementsByClassName("color-button")
-    
+
+    function changeColor(button) {
+        button.addEventListener("click", (event) => {
+            var clickedButton = event.target
+            var clickedColor = clickedButton.getAttribute("data-color")
+            var clickedGrandparent = clickedButton.parentElement.parentElement
+            var elementToChange = clickedGrandparent.getAttribute("data-panel")
+            var toggleButtons = document.querySelectorAll("button[data-panel]")//Buttons which, when click expand and collapse their respective color panels
+            if (document.getElementById(elementToChange)) {
+                document.getElementById(elementToChange).style.backgroundColor = clickedColor;//Changes the color of the element that corresponds to the currently expanded panel based on the color button clicked by the user inside that panel.
+            } else {//Since there is more than one hand, getting them all using an ID would not be possible
+                const ALL_HANDS = document.getElementsByClassName(elementToChange)
+                Array.prototype.forEach.call(ALL_HANDS, hand => {
+                    hand.style.backgroundColor = clickedColor;
+                }
+                )
+            }
+            if (elementToChange == "interface") {//When the user changes the interface color, the text for the color buttons and toggle buttons will be changed to that color as well
+                Array.prototype.forEach.call(toggleButtons, (button) => button.style.color = clickedColor)
+                Array.prototype.forEach.call(COLOR_BUTTONS, button => button.style.color = clickedColor)
+            }
+        });
+    }
+
     // Looping through the COLOR_BUTTONS NodeList to add a click listener to each.
     Array.prototype.forEach.call(
-        COLOR_BUTTONS, (button)=> {
-            button.addEventListener("click", (event)=>{
-                var clickedButton = event.target
-                var clickedColor = clickedButton.getAttribute("data-color")
-                var clickedGrandparent = clickedButton.parentElement.parentElement
-                var elementToChange = clickedGrandparent.getAttribute("data-panel")
-                var toggleButtons = document.querySelectorAll("button[data-panel]")//Buttons which, when click expand and collapse their respective color panels
-                if(document.getElementById(elementToChange)){
-                    document.getElementById(elementToChange).style.backgroundColor = clickedColor;//Changes the color of the element that corresponds to the currently expanded panel based on the color button clicked by the user inside that panel.
-                } else {//Since there is more than one hand, getting them all using an ID would not be possible
-                    const ALL_HANDS = document.getElementsByClassName(elementToChange)
-                    Array.prototype.forEach.call(ALL_HANDS, hand => {
-                            hand.style.backgroundColor = clickedColor;
-                        }
-                    )
-                }
-                if(elementToChange == "interface"){//When the user changes the interface color, the text for the color buttons and toggle buttons will be changed to that color as well
-                    Array.prototype.forEach.call(toggleButtons, (button)=> button.style.color = clickedColor)
-                    Array.prototype.forEach.call(COLOR_BUTTONS, button => button.style.color = clickedColor)
-                } 
-            });
-        }
+        COLOR_BUTTONS, changeColor
     )
-    CHANGE_CLOCK_COLORS.addEventListener("click", toggleColorPanels)
+
+    SUBDIV_TOGGLE_BUTTONS.addEventListener("click", toggleColorPanels)
+    
     setInterval(setTime, 1000)
 }
